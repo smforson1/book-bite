eab2a485a3d79b0aa34adbfc6e05953d56389d02
smforson1 <drmidnight44@gmail.com>
2026-01-03 16:54:33 +0000

    style: change More tab icon to horizontal dots

--- mobile/src/components/navigation/CustomTabBar.tsx
+++ mobile/src/components/navigation/CustomTabBar.tsx
@@ -53,7 +53,7 @@ export default function CustomTabBar({ state, descriptors, navigation }: BottomT
                     }
                     if (route.name === 'Orders') iconName = 'clipboard-list';
                     if (route.name === 'Wallet') iconName = 'wallet';
-                    if (route.name === 'More') iconName = 'dots-vertical';
+                    if (route.name === 'More') iconName = 'dots-horizontal';
 
                     const animatedIconStyle = useAnimatedStyle(() => {
                         return {
